31b17e29e9ff27d3361dc885e3e847aae2ad9c86
getsanjeev <getsanjeevdubey@gmail.com>
2017-11-20 03:47:56 +0530

    added final script of write

--- write.py
+++ write.py
@@ -7,20 +7,37 @@ import math
 # import zigzag functions
 from zigzag import *
 
+
+def get_run_length_encoding(image):
+    i = 0
+    skip = 0
+    stream = []    
+    bitstream = ""
+    image = image.astype(int)
+    while i < image.shape[0]:
+        if image[i] != 0:            
+            stream.append((image[i],skip))
+            bitstream = bitstream + str(image[i])+ " " +str(skip)+ " "
+            skip = 0
+        else:
+            skip = skip + 1
+        i = i + 1
+
+    return bitstream
+
 # defining block size
 block_size = 8
 
 # Quantization Matrix
-QUANTIZATION_MAT = [[16,11,10,16,24,40,51,61],[12,12,14,19,26,58,60,55],[14,13,16,24,40,57,69,56 ],[14,17,22,29,51,87,80,62],[18,22,37,56,68,109,103,77],[24,35,55,64,81,104,113,92],[49,64,78,87,103,121,120,101],[72,92,95,98,112,100,103,99]] 
+QUANTIZATION_MAT = np.array([[16,11,10,16,24,40,51,61],[12,12,14,19,26,58,60,55],[14,13,16,24,40,57,69,56 ],[14,17,22,29,51,87,80,62],[18,22,37,56,68,109,103,77],[24,35,55,64,81,104,113,92],[49,64,78,87,103,121,120,101],[72,92,95,98,112,100,103,99]])
 print(QUANTIZATION_MAT)
 
 # reading image in grayscale
-img = cv2.imread('pdf.png', 0)
+img = cv2.imread('emma.png', cv2.IMREAD_GRAYSCALE)
+#img = np.array([[255,255,227,204,204,203,192,217],[215,189,167,166,160,135,167,244],[169,115,99,99,99,82,127,220],[146,90,86,88,84,63,195,189],[255,255,231,239,240,182,251,232],[255,255,21,245,226,169,229,247],[255,255,222,251,174,209,174,163],[255,255,221,184,205,248,249,220]])
 
 print("Image read : ")
 print(img)
-
-#cv2.imshow('input image', img)
 
 # get size of the image
 [h , w] = img.shape
@@ -32,8 +49,8 @@ print(img)
 # you need to convert h and w to float to get the right number
 height = h
 width = w
-h = np.float32(h)##### your code #####
-w = np.float32(w)##### your code #####
+h = np.float32(h) ##### your code #####
+w = np.float32(w) ##### your code #####
 
 # to cover the whole image the number of blocks should be ceiling of the division of image size by block size
 # at the end convert it to int
@@ -71,10 +88,11 @@ for i in range(height):
 
 print("Padded images :")
 print(padded_img.astype(int))
+cv2.imwrite('uncompressed.bmp', np.uint8(padded_img))
 
 #cv2.imshow('input padded image', np.uint8(padded_img).astype(int))
 
-##################### step 3 #####################
+
 # start encoding:
 # divide image into block size by block size (here: 8-by-8) blocks
 # To each block apply 2D discrete cosine transform
@@ -86,63 +104,71 @@ print(padded_img.astype(int))
 for i in range(nbh):
     
         # Compute start row index of the block
-        row_ind_1 = i*block_size##### your code #####
+        row_ind_1 = i*block_size
         
         # Compute end row index of the block
-        row_ind_2 = row_ind_1+block_size##### your code #####
+        row_ind_2 = row_ind_1+block_size
         
         for j in range(nbw):
             
             # Compute start column index of the block
-            col_ind_1 = j*block_size##### your code #####
+            col_ind_1 = j*block_size
             
             # Compute end column index of the block
-            col_ind_2 = col_ind_1+block_size##### your code #####
+            col_ind_2 = col_ind_1+block_size
             
             # select the current block we want to process using calculated indices
             block = padded_img[ row_ind_1 : row_ind_2 , col_ind_1 : col_ind_2 ]
-
-            if(j == 0 and i == 0):
-                print(block)
-            
+                       
             # apply 2D discrete cosine transform to the selected block
             # you should use opencv dct function
-            DCT = cv2.dct(block)##### your code #####
+            print("BLOCK : ",i,j)
+            print(block)
+            DCT = cv2.dct(block)
 
-            print("Box : DCT")
-            print(DCT)
+            print("Box : DCT",i,j)
+            print(DCT.astype(int))
 
             DCT_normalized = np.divide(DCT,QUANTIZATION_MAT).astype(int)
+            print("AFTER QUantization")
             print(DCT_normalized)
             
             # reorder DCT coefficients in zig zag order by calling zigzag function
             # it will give you a one dimentional array
-            reordered = zigzag(DCT_normalized)##### your code #####
-
-            print("ZIGZAG")
-            print(reordered)
-            
+            reordered = zigzag(DCT_normalized)                      
             # reshape the reorderd array back to (block size by block size) (here: 8-by-8)
-            reshaped= np.reshape(reordered, (block_size, block_size)) ##### your code #####            
+            reshaped= np.reshape(reordered, (block_size, block_size)) 
             
             # copy reshaped matrix into padded_img on current block corresponding indices
-            padded_img[row_ind_1 : row_ind_2 , col_ind_1 : col_ind_2] = reshaped##### your code #####
+            padded_img[row_ind_1 : row_ind_2 , col_ind_1 : col_ind_2] = reshaped
+            print()
+            print()
 
 cv2.imshow('encoded image', np.uint8(padded_img))
 
-
-
 print("DCT matrix FINAL")
 print(padded_img.astype(int))
+arranged = padded_img.flatten()
+#arranged = np.reshape(padded_img.astype(int),(1,padded_img.size))
+print(arranged.astype(int))
+print()
+bitstream = get_run_length_encoding(arranged)
+bitstream = str(padded_img.shape[0]) + " " + str(padded_img.shape[1]) + " " + bitstream + ";"
+
+print("Bitstream : ")
+print(bitstream)
 
 ##################### step 4 #####################
 # write h, w, block_size and padded_img into txt files at the end of encoding
 
 # write padded_img into 'encoded.txt' file. You can use np.savetxt function.
-np.savetxt('encoded.txt',padded_img)##### your code #####
+#np.savetxt('encoded.txt',bitstream)##### your code #####
 
 # write [h, w, block_size] into size.txt. You can use np.savetxt function.
-np.savetxt('size.txt',[h, w, block_size])##### your code #####
+#np.savetxt('size.txt',[h, w, block_size])##### your code #####
+file1 = open("image.txt","w")
+file1.write(bitstream)
+file1.close()
 
 ##################################################
 
